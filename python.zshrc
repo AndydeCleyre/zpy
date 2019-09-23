AndydeCleyre/zpy

commit b7de29162d41b6c95b533cf219f84c1583e91a04

rewrite _get_sublp (python -> zsh)

python.zshrc
@@ -258,17 +258,15 @@ if pyproject.is_file():
 
 # get a new or existing sublime text project file for the working folder
 _get_sublp () {
-    python -c "
-from pathlib import Path
-
-cwd = Path().absolute()
-try:
-    spfile = next(cwd.glob('*.sublime-project'))
-except StopIteration:
-    spfile = cwd / f'{cwd.name}.sublime-project'
-    spfile.write_text('{}')
-print(spfile, end='')
-    "
+	local spfile
+	spfile="$(ls *.sublime-project(N:P) | head -1)"
+	local folder
+	folder=$(pwd)
+	if [[ ! $spfile ]]; then
+		spfile="${folder}/${folder:t}.sublime-project"
+		printf '{}' > $spfile
+	fi
+	printf $spfile
 }
 
 # specify the venv interpreter in a new or existing sublime text project file

commit 0969ec5bfeff21973180d8a43526c1becbddfc54

fix naming of newly created sublime project file (bug introduced by b7de29162d41b6c95b533cf219f84c1583e91a04 )

python.zshrc
@@ -260,15 +260,15 @@ if pyproject.is_file():
 
 # get a new or existing sublime text project file for the working folder
 _get_sublp () {
-	local spfile
-	spfile="$(ls *.sublime-project(N:P) | head -1)"
-	local folder
-	folder=$(pwd)
-	if [[ ! $spfile ]]; then
-		spfile="${folder}/${folder:t}.sublime-project"
-		printf '{}' > $spfile
-	fi
-	printf $spfile
+    local spfile
+    spfile="$(ls *.sublime-project(:P) | head -1)" 2> /dev/null
+    local folder
+    folder=$(pwd)
+    if [[ ! $spfile ]]; then
+        spfile="${folder}/${folder:t}.sublime-project"
+        printf '{}' > $spfile
+    fi
+    printf $spfile
 }
 
 # specify the venv interpreter in a new or existing sublime text project file