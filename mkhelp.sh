17cf0c1817e549235c3a910b61a5747521b8e15e
Andy Kluger <andydecleyre@gmail.com>
2019-11-08 17:04:24 -0500

    use pcregrep and new patterns from main app in mkhelp.sh

--- mkhelp.sh
+++ mkhelp.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-grep -P \
-    '(^#[^!])|(^(alias|local))|(^$)|(^\S+ \(\) {(.*})?(  # .+)?$)' \
-    "${1:-$(dirname "$0")/python.zshrc}" | \
-    uniq | \
-    sed -E 's/(local )?(.+) \(\) \{([^\}]*\})?(.*)/\2\4/g' | \
-    sed -E 's/^alias ([^=]+).+(  # .+)/\1\2/g'
-
+pcregrep '^(alias|([^ \n]+ \(\))|#|$)' "${1:-$(dirname "$0")/python.zshrc}" \
+| uniq \
+| sed -E 's/(^[^ ]+) \(\) \{(.*\})?(.*)/\1\3/g' \
+| sed -E 's/^alias ([^=]+)[^#]+(# .+)?/\1  \2/g'
 # select leading comments, aliases & function openers, and empty lines
 # squeeze repeats (newlines)
 # drop any function content other than name and comment
