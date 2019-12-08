AndydeCleyre/zpy

commit a09fa2aa6d88c200e441f43224b5dbe3e93519c2

activate-ing rather than envin-ing at this point during pipz (re)install should save time and work sometimes, while never doing worse

python.zshrc
@@ -431,8 +431,12 @@ sublp () {  # [subl-arg...]
                  | jq -r '.[] | select(.name=="'${${pdir:t}//[^[:alnum:].]##/-}'") | .name,.version'
              ))
          else
-             local piplistline=($(vpyfrom $pdir pip list | grep "^${${pdir:t}//[^[:alnum:].]##/-} "))
+             local piplistline=($(
+                 vpyfrom $pdir pip list \
+                 | grep "^${${pdir:t}//[^[:alnum:].]##/-} "
+             ))
          fi
+         piplistline+=('????')
          print -rl "${bin:t}" "${piplistline[1,2]}" "$(vpyfrom $pdir python -V)"
      fi
  }
@@ -443,8 +447,8 @@ sublp () {  # [subl-arg...]
      local pkg=$2
      mkdir -p $projects_home/$pkg
      cd $projects_home/$pkg
-     envin
      rm -f requirements.{in,txt}
+     activate
      pipacs $pkg
      envout
  }

commit b0b7fcb66bef5ce17fbca350bff4b29a00b39491

envin if necessary during pipz install; fixes bug introduced by a09fa2aa6d88c200e441f43224b5dbe3e93519c2

python.zshrc
@@ -448,7 +448,7 @@ sublp () {  # [subl-arg...]
      mkdir -p $projects_home/$pkg
      cd $projects_home/$pkg
      rm -f requirements.{in,txt}
-     activate
+     activate 2>/dev/null || envin
      pipacs $pkg
      envout
  }