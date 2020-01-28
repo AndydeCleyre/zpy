0b697f65f2f1f98e6ae45e87ba775145e9b4609e
Andy Kluger <andykluger@gmail.com>
2020-01-28 17:02:23 -0500

    in make scripts that install python packages, warn when no venv activated

--- mk/site.sh
+++ mk/site.sh
@@ -2,6 +2,8 @@
 trap "cd $PWD" EXIT
 cd "$(dirname "$0")"
 
+[[ $VIRTUAL_ENV ]] \
+    || echo -e "You may want to activate a venv first in order to install and use the build tools.\nTrying anyway . . ."
 pip install -qr ../doc/doc-requirements.txt
 
 ./help.sh
