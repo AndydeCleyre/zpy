beca5832990553108ae78c909075be2249a925c9
Andy Kluger <andykluger@gmail.com>
2020-08-11 12:43:35 -0400

    version bump in sphinx conf; strip comments about inactive sphinx themes

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -1,18 +1,9 @@
 project = 'zpy'
 author = 'Andy Kluger'
-release = '0.0.4'
-html_theme = 'bootstrap'
+release = '0.0.5'
 html_favicon = '../favicon-32x32.png'
-
-# html_sidebars = {'**': ['localtoc.html']}
+html_theme = 'bootstrap'
 html_theme_options = {
-    # alabaster:
-    # 'github_user': 'andydecleyre',
-    # 'github_repo': 'zpy',
-    # 'github_banner': True,
-    # 'fixed_sidebar': True,
-
-    # boostrap:
     'navbar_pagenav_name': "Jump to …",
     'source_link_position': 'nowhere',
     'bootswatch_theme': 'flatly',
@@ -22,8 +13,4 @@ html_theme_options = {
         ("Issues", 'https://github.com/andydecleyre/zpy/issues', True),
         ("pip-tools", 'https://github.com/jazzband/pip-tools', True),
     ],
-
-    # pydata_sphinx_theme:
-    # 'github_url': 'https://github.com/andydecleyre/zpy',
-    # 'search_bar_position': 'nowhere',
 }
