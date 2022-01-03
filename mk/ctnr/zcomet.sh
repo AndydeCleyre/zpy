49578db0f63639339deae82553105e58d14d708b
Andy Kluger <AndyKluger@gmail.com>
2022-01-03 16:46:49 -0500

    rework container flows:
    - zim -> zcomet
    - semaphoreci -> gh actions
    - remove automated svg creation stuff

---
+++ mk/ctnr/zcomet.sh
@@ -0,0 +1,128 @@
+#!/bin/sh -e
+# [fedora|alpine|ubuntu=fedora [<base-tag>]]
+
+distro=${1:-fedora}
+
+ctnr=zcomet-$distro
+img=quay.io/andykluger/$ctnr
+user=dev
+
+ctnr_run () {  # [-u] <cmd> [<cmd-arg>...]
+  _u=root
+  if [ "$1" = -u ]; then _u=$user; shift; fi
+  buildah run --user $_u "$ctnr" "$@"
+}
+
+ctnr_append () {  # [-u] <dest-path>
+  unset _u
+  if [ "$1" = -u ]; then _u=-u; shift; fi
+  ctnr_run $_u sh -c "cat >>$1"
+}
+
+pkgs=zsh  # ca-certificates git less sudo wget
+fat="/home/${user}/.zcomet/repos/*/*/.git"
+case $distro in
+  fedora)
+    basetag=${2:-35}
+    pkgs="$pkgs git-core"
+    fat="$fat /var/cache/* /var/log/* /usr/lib64/python3.10/__pycache__"
+    alias ctnr_pkg="ctnr_run dnf -yq --setopt=install_weak_deps=False"
+    alias ctnr_pkg_upgrade="ctnr_pkg distro-sync"
+    alias ctnr_pkg_add="ctnr_pkg install"
+    alias ctnr_mkuser="ctnr_run useradd -m -s /bin/zsh"
+  ;;
+  alpine)
+    basetag=${2:-3.15}
+    pkgs="$pkgs git sudo"
+    fat="$fat /var/cache/apk/*"
+    alias ctnr_pkg="ctnr_run apk -q --no-progress"
+    alias ctnr_pkg_upgrade="ctnr_pkg upgrade"
+    alias ctnr_pkg_add="ctnr_pkg add"
+    alias ctnr_mkuser="ctnr_run adduser -D -s /bin/zsh"
+  ;;
+  ubuntu)
+    basetag=${2:-20.04}
+    pkgs="$pkgs ca-certificates less git sudo wget"
+    fat="$fat /var/cache/* /var/lib/apt/lists/*"
+    alias ctnr_pkg="ctnr_run apt -yqq --no-install-recommends"
+    alias ctnr_pkg_upgrade="ctnr_pkg update && ctnr_pkg full-upgrade"
+    alias ctnr_pkg_add="ctnr_pkg install"
+    alias ctnr_mkuser="ctnr_run useradd -m -s /bin/zsh"
+  ;;
+  *)
+    printf '%s\n' 'Args: [fedora|alpine|ubuntu=fedora [<base-tag>]]'
+    exit 1
+  ;;
+esac
+
+# Start fresh
+buildah rm "$ctnr" 2>/dev/null || true
+buildah from -q --name "$ctnr" "docker.io/library/$distro:$basetag"
+
+# Upgrade and install packages
+ctnr_pkg_upgrade
+# shellcheck disable=SC2086
+ctnr_pkg_add $pkgs
+
+# Add user with sudo powers
+ctnr_mkuser $user
+printf '%s\n' "$user ALL=(ALL) NOPASSWD: ALL" \
+| ctnr_append /etc/sudoers.d/$user
+
+# Install zcomet
+ctnr_run -u git clone -q --depth 1 https://github.com/agkozak/zcomet /home/$user/zcomet
+
+# Add settings and plugins to .zshrc:
+ctnr_run -u rm -f /home/$user/.zshrc
+<<EOF ctnr_append -u /home/$user/.zshrc
+typeset -U path=(~/.local/bin \$path)
+precmd () { rehash }
+
+. ~/zcomet/zcomet.zsh
+() {
+  for plugin (
+    agkozak/agkozak-zsh-prompt
+    zdharma-continuum/fast-syntax-highlighting
+    zimfw/environment
+    zimfw/input
+    zsh-users/zsh-autosuggestions
+    zsh-users/zsh-completions
+  ) zcomet load \$plugin
+}
+zcomet compinit
+
+setopt HIST_IGNORE_ALL_DUPS
+
+AGKOZAK_LEFT_PROMPT_ONLY=1
+WORDCHARS=\${WORDCHARS//[\/]}
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=fg=4
+
+# Good History:
+autoload -Uz history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end  history-search-end
+bindkey '^[[A' history-beginning-search-backward-end
+bindkey '^[[B' history-beginning-search-forward-end
+if [[ \${terminfo[kcuu1]} && \${terminfo[kcud1]} ]] {
+  bindkey \${terminfo[kcuu1]} history-beginning-search-backward-end
+  bindkey \${terminfo[kcud1]} history-beginning-search-forward-end
+}
+
+EOF
+
+# Fetch plugins
+ctnr_run -u zsh -ic exit
+
+# Cut some fat
+ctnr_run sh -c "rm -rf $fat"
+
+# Set default user, working dir, TERM, and command
+buildah config \
+  --user $user \
+  --workingdir /home/$user \
+  --env TERM=xterm-256color \
+  --cmd zsh \
+  "$ctnr"
+
+# Press container as an image, tagged with today's date
+buildah tag "$(buildah commit -q --rm "$ctnr" "$img")" "$img:$(date +%Y.%j)"
--- mk/ctnr/zpy.sh
+++ mk/ctnr/zpy.sh
@@ -1,74 +1,86 @@
 #!/bin/sh -e
-# --local|<branch> [<base-distro>=fedora]
+# --local|<branch> [fedora|alpine|ubuntu=fedora]
 
+# Set branch and distro
 zpy_branch=$1
-shift
-if [ ! $zpy_branch ] || [ $zpy_branch = -h ] || [ $zpy_branch = --help ]; then
-  printf '%s\n' \
-    'Args: --local|<branch> [<base-distro>]' \
-    'Accepted distros:' \
-    'fedora (default)' alpine ubuntu
+if [ ! "$zpy_branch" ] || [ "$zpy_branch" = -h ] || [ "$zpy_branch" = --help ]; then
+  printf '%s\n' 'Args: --local|<branch> [fedora|alpine|ubuntu=fedora]'
   exit 1
 fi
+shift
 distro=${1:-fedora}
 
 ctnr=zpy-$distro
 img=quay.io/andykluger/$ctnr
 user=dev
 
-base_img=quay.io/andykluger/zim-$distro
+base_img=quay.io/andykluger/zcomet-$distro
 today="$(date +%Y.%j)"
 gitroot="$(git -C "$(dirname "$0")" rev-parse --show-toplevel)"
 
-alias ctnr_run="buildah run --user root $ctnr"
-alias ctnr_urun="buildah run --user $user $ctnr"
+ctnr_run () {  # [-u] <cmd> [<cmd-arg>...]
+  _u=root
+  if [ "$1" = -u ]; then _u=$user; shift; fi
+  buildah run --user $_u "$ctnr" "$@"
+}
+
+ctnr_append () {  # [-u] <dest-path>
+  unset _u
+  if [ "$1" = -u ]; then _u=-u; shift; fi
+  ctnr_run $_u sh -c "cat >>$1"
+}
+
+pkgs='fzf python3'            # minimal, common
+pkgs="$pkgs highlight micro"  # recommended, common
+fat="/home/${user}/.zcomet/repos/*/*/.git /usr/lib*/python3.*/__pycache__"
 case $distro in
   fedora)
-    pkgs='diffutils fzf highlight micro pcre-tools python3'
-    fat='/var/cache/* /var/log/* /usr/lib64/python3.8/__pycache__'
+    pkgs="$pkgs diffutils pcre-tools"  # minimal
+    pkgs="$pkgs gcc python3-devel"     # numpy, pandas, etc.
+    fat="$fat /var/cache/* /var/log/*"
     alias ctnr_pkg="ctnr_run dnf -yq --setopt=install_weak_deps=False"
     alias ctnr_pkg_upgrade="ctnr_pkg distro-sync"
     alias ctnr_pkg_add="ctnr_pkg install"
   ;;
   alpine)
-    pkgs='fzf highlight pcre2-tools python3'
-    micro_version="2.0.6"
-    fat='/var/cache/apk/*'
+    pkgs="$pkgs pcre2-tools"               # minimal
+    pkgs="$pkgs gcc python3-dev musl-dev"  # numpy, pandas, etc.
+    fat="$fat /var/cache/apk/*"
     alias ctnr_pkg="ctnr_run apk -q --no-progress"
     alias ctnr_pkg_upgrade="ctnr_pkg upgrade"
     alias ctnr_pkg_add="ctnr_pkg add"
   ;;
   ubuntu)
-    pkgs='fzf highlight micro pcre2-utils python3 python3-venv'
-    fat='/var/cache/* /var/lib/apt/lists/*'
+    pkgs="$pkgs pcre2-utils python3-venv"  # minimal
+    fat="$fat /var/cache/* /var/lib/apt/lists/*"
     alias ctnr_pkg="ctnr_run apt -yqq --no-install-recommends"
     alias ctnr_pkg_upgrade="ctnr_pkg update && ctnr_pkg full-upgrade"
     alias ctnr_pkg_add="ctnr_pkg install"
   ;;
   *)
-    printf '%s\n' \
-      'Args: --local|<branch> [<base-distro>]' \
-      'Accepted distros:' \
-      'fedora (default)' alpine ubuntu
+    printf '%s\n' 'Args: --local|<branch> [fedora|alpine|ubuntu=fedora]'
     exit 1
   ;;
 esac
 
 # Start fresh
-buildah rm $ctnr 2>/dev/null || true
-if ! buildah from -q --name $ctnr "$base_img:$today" 2>/dev/null; then
-  "${gitroot}/mk/ctnr/zim.sh" $distro
-  buildah from -q --name $ctnr "$base_img:$today"
+buildah rm "$ctnr" 2>/dev/null || true
+if ! buildah from -q --name "$ctnr" "$base_img:$today" 2>/dev/null; then
+  "${gitroot}/mk/ctnr/zcomet.sh" "$distro"
+  buildah from -q --name "$ctnr" "$base_img:$today"
 fi
 
 # Upgrade and install packages
 ctnr_pkg_upgrade
+# shellcheck disable=SC2086
 ctnr_pkg_add $pkgs
 
-# Set EDITOR and convenient alias
-ctnr_urun sh -c 'printf "%s=micro\n" \
-  EDITOR "alias e" \
->> ~/.zshrc'
+# Set variables
+<<EOF ctnr_append -u /home/${user}/.zshenv
+export EDITOR='micro'
+export HIGHLIGHT_OPTIONS='-O truecolor -s ekvoli -t 4 --force --stdout'
+export LESS='ij.3JRWX'
+EOF
 
 # Install zpy
 if [ "$zpy_branch" = --local ]; then
@@ -77,67 +89,70 @@ if [ "$zpy_branch" = --local ]; then
     printf '%s-local-working' "$host"
   )"
   zpy_version="$(git -C "$gitroot" describe --dirty)"
-  buildah copy --chown $user $ctnr "${gitroot}/zpy.plugin.zsh" /home/$user/
-  ctnr_urun sh -c 'printf "%s\n" \
-    ". ~/zpy.plugin.zsh" \
-  >> ~/.zshrc'
+  buildah copy --chown "$user" "$ctnr" "${gitroot}/zpy.plugin.zsh" /home/${user}/
+  printf '%s\n' '. ~/zpy.plugin.zsh' \
+  | ctnr_append -u /home/${user}/.zshrc
 else
-  ctnr_urun zsh -ic "print -l \
-    'zmodule andydecleyre/zpy -b $zpy_branch' \
-  >> ~/.zimrc; zimfw install"
-  zpy_version="$(ctnr_urun git -C /home/$user/.zim/modules/zpy describe --dirty)"
+  ctnr_run -u sed -Ei \
+    "s:^(zcomet compinit( .*)?)$:zcomet load andydecleyre/zpy@${zpy_branch}\n\1:" \
+    /home/${user}/.zshrc
+  ctnr_run -u zsh -ic exit
+  zpy_version="$(ctnr_run -u git -C /home/${user}/.zcomet/repos/andydecleyre/zpy describe --dirty)"
 fi
 printf 'zpy_branch: %s\n' "$zpy_branch"
 printf 'zpy_version: %s\n' "$zpy_version"
 
 # Install standalone vpy script, for simpler shebangs
-ctnr_urun mkdir -p /home/$user/.local/bin
-ctnr_urun zsh -ic '.zpy_mkbin vpy ~/.local/bin/vpy'
+ctnr_run -u mkdir -p /home/${user}/.local/bin
+ctnr_run -u zsh -ic '.zpy_mkbin vpy ~/.local/bin/vpy'
 
-# Inform user about potential PyPI package build deps, Py2 support, EDITOR
+# Inform user about Py2 support
 case $distro in
   fedora)
-    ctnr_urun sh -c 'printf "print %s\n" \
-      "-P \"%F{blue}# For python2 support:%f\"" \
-      "sudo dnf --setopt=install_weak_deps=False install python2 python3-virtualenv" \
-      "-P \"%F{blue}# For pip to successfully install some packages like numpy, pandas:%f\"" \
-      "sudo dnf --setopt=install_weak_deps=False install gcc python3-devel" \
-      "-P \"%F{blue}# micro editor is installed, and aliased as e%f\"" \
-    >> ~/.zshrc'
+    <<EOF ctnr_append -u /home/${user}/.zshrc
+print -rl '# For python2 support:' \\
+  'sudo dnf --setopt=install_weak_deps=False install python2 python3-virtualenv' \\
+| .zpy_hlt zsh
+EOF
   ;;
   alpine)
-    ctnr_run sh -c "wget -O - \
-      https://github.com/zyedidia/micro/releases/download/v${micro_version}/micro-${micro_version}-linux64-static.tar.gz \
-      | tar x -z -O -f - micro-${micro_version}/micro \
-    > /usr/local/bin/micro; chmod +x /usr/local/bin/micro"
-    ctnr_urun sh -c 'printf "print %s\n" \
-      "-P \"%F{blue}# For python2 support:%f\"" \
-      "sudo apk add python2" \
-      "sudo python2 -m ensurepip" \
-      "sudo pip2 install virtualenv" \
-      "-P \"%F{blue}# For pip to successfully install some packages like numpy, pandas:%f\"" \
-      "sudo apk add gcc {python3,musl}-dev" \
-      "-P \"%F{blue}# For pillow, additionally:%f\"" \
-      "sudo apk add {jpeg,zlib}-dev" \
-      "-P \"%F{blue}# micro editor is installed, and aliased as e%f\"" \
-    >> ~/.zshrc'
+    <<EOF ctnr_append -u /home/${user}/.zshrc
+print -rl '# For python2 support:' \\
+  'sudo apk add python2' \\
+  'sudo python2 -m ensurepip' \\
+  'sudo pip2 install virtualenv' \\
+| .zpy_hlt zsh
+EOF
   ;;
   ubuntu)
-    ctnr_urun sh -c 'printf "print %s\n" \
-      "-P \"%F{blue}# For python2 support:%f\"" \
-      "sudo apt --no-install-recommends install python2 virtualenv" \
-      "-P \"%F{blue}# micro editor is installed, and aliased as e%f\"" \
-    >> ~/.zshrc'
+    <<EOF ctnr_append -u /home/${user}/.zshrc
+print -rl '# For python2 support:' \\
+  'sudo apt --no-install-recommends install python2 virtualenv' \\
+| .zpy_hlt zsh
+EOF
   ;;
 esac
 
+# Set aliases
+<<EOF ctnr_append -u /home/${user}/.zshrc
+alias e="\$EDITOR"
+alias h="highlight"
+EOF
+
+# Print aliases
+<<EOF ctnr_append -u /home/${user}/.zshrc
+print -rl '' '# Aliases:' | .zpy_hlt zsh
+grep '^alias ' \$HOME/.zshrc | .zpy_hlt zsh
+EOF
+
 # Remind user of a few useful commands
-ctnr_urun sh -c 'printf "%s\n" \
-  "zpy zpy envin pipacs pipz" \
->> ~/.zshrc'
+<<EOF ctnr_append -u /home/${user}/.zshrc
+print -l
+zpy zpy envin pipacs pipz
+EOF
 
 # Cut some fat
 ctnr_run sh -c "rm -rf $fat"
 
 # Press container as an image, tagged with zpy version and branch
-buildah tag "$(buildah commit --quiet --rm $ctnr $img)" "$img:$zpy_version" "$img:$zpy_branch"
+buildah tag "$(buildah commit --quiet --rm "$ctnr" "$img")" "$img:$zpy_version" "$img:$zpy_branch"
