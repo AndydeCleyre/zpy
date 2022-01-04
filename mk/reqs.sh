6aad7650eb3968492efd6c2595b77fbdc1f08797
Andy Kluger <AndyKluger@gmail.com>
2022-01-04 13:32:12 -0500

    add reqs bumping and mkdocs reqs

---
+++ mk/reqs.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -e
+
+if [ "$1" ]; then
+  printf '%s\n' 'Upgrade what we can in our *requirements.txt files' 'Args: None' 1>&2
+  exit 1
+fi
+
+gitroot="$(git -C "$(dirname "$0")" rev-parse --show-toplevel)"
+cd "$gitroot"
+
+if [ ! -d venv ]; then
+  python3 -m venv venv
+fi
+# shellcheck disable=SC1091
+. ./venv/bin/activate
+pip install -qU pip-tools
+
+for folder in doc/mkdocs; do
+  cd "$folder"
+  for reqsin in *requirements.in; do
+    pip-compile -U --no-header --annotation-style=line "$reqsin"
+    printf '%s\n' "Wrote lockfile for ${folder}/${reqsin}"
+    git status --short "${reqsin}"
+  done
+done
