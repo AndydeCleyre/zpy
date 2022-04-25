10335d2fa4320bfb26d29a67f58e2a3d432bcb4c
Andy Kluger <AndyKluger@gmail.com>
2022-04-25 12:43:12 -0400

    update ubuntu base image 20.04->22.04

--- mk/ctnr/zcomet.sh
+++ mk/ctnr/zcomet.sh
@@ -41,7 +41,7 @@ case $distro in
     alias ctnr_mkuser="ctnr_run adduser -D -s /bin/zsh"
   ;;
   ubuntu)
-    basetag=${2:-20.04}
+    basetag=${2:-22.04}
     pkgs="$pkgs ca-certificates less git sudo wget"
     fat="$fat /var/cache/* /var/lib/apt/lists/*"
     alias ctnr_pkg="ctnr_run apt -yqq --no-install-recommends"
